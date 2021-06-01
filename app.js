92af419824c34e87885f90f6d0aae3f412924638
Jazer <contact@jazer.co.uk>
2021-06-01 15:57:00 +0100

    Entropy now working too :D

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ window.addEventListener("load", function(){
         result.textContent = "Result: " + calculateGamma(document.querySelector("#gamma").value, document.querySelector("#reward").value);
     }
 
-})
 
+    let entropyButton = document.querySelector("#btnEntropy");
+    entropyButton.onclick = () => {
+        let result = document.querySelector("#lblEntropyResult")
+
+        let yn = document.querySelector("#yesVno").value;
+        yn = yn.replace(/\s+/g, '');
+
+        let yes = parseInt(yn.split(",")[0]);
+        let no = parseInt(yn.split(",")[1]);
+
+        result.textContent = "Result: " + calculateEntropy(yes, no);
+    }
+    
+})
 
 function calculateGamma(gamma, rewards) {
     // Remove white space
@@ -23,4 +36,12 @@ function calculateGamma(gamma, rewards) {
         else result += Math.pow(gamma,i)*ar[i];
     }
     return result;
+}
+
+function calculateEntropy(yes, no) {
+    let tot = yes + no;
+    if (yes == 0 || no == 0) return 0;
+    if (yes == no) return 1;
+    let res = -(yes/tot)*Math.log2(yes/tot)-(no/tot)*Math.log2(no/tot)
+    return res;
 }
